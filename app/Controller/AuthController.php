529d57bcf5f84113a1549fc3a7c23aa54c1422e0
myusername <myusername@myemaildomain.com>
2018-09-29 23:49:01 +0400

    fgfgfgfgffffffffg

---
+++ app/Controller/AuthController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 29.09.2018
+ * Time: 22:38
+ */
+
+namespace Controller\API\V001;
+
+use System\Controller\AbstractController;
+
+class AuthController extends AbstractController
+{
+	public function accessTokenAction()
+	{
+
+	}
+
+	public function refreshTokenAction()
+	{
+
+	}
+}
---
+++ app/Controller/LogoutController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 29.09.2018
+ * Time: 22:47
+ */
+
+namespace Controller\API\V001;
+
+use System\Controller\AbstractController;
+
+class LogoutController extends AbstractController
+{
+	public function logoutAction()
+	{
+
+	}
+}
